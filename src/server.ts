d053026abfbcf91857cf46058d08784f0059022d
CezzMartins <cezzmartins@gmail.com>
2022-06-18 18:01:03 -0300

    Init: initialize

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+import path from "path";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const server = express();
+
+server.use(express.static(path.join(__dirname, '../public')));
+server.use(express.urlencoded({extended: true}));
+
+
+server.use((request: Request, response: Response) => {
+    response.status(404);
+    response.json({
+        error: 'Endpoint Não Encontrato.'
+    });
+})
+
+
+server.listen(process.env.PORT);
