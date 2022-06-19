b32136140b83c379efe2b272399d151432e07ba7
CezzMartins <cezzmartins@gmail.com>
2022-06-19 01:46:39 -0300

    feat: remove extra code

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -3,12 +3,6 @@ import * as apiController from '../controllers/apiController';
 
 const router = Router();
 
-router.get('/ping', apiController.ping);
-
-router.get('/radomnumber', apiController.random);
-
-router.get('/nameuser/:name', apiController.name);
-
 router.get('/phrases', apiController.phrases);
 
 router.get('/phrase/:id', apiController.getPhraseById)
@@ -20,17 +14,6 @@ router.post('/create', apiController.create);
 router.put('/update/:id', apiController.updatePhrase);
 
 router.delete('/delete/:id', apiController.deletePhrase);
-
-
-
-
-
-
-
-
-
-
-
 
 
 export default router;
